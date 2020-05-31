d25d4fb38e38785d3e7c0d43e66016983388d93d
Venk <VenkSociety@users.noreply.github.com>
2020-05-31 19:34:28 +1000

    Make script scan multiple URLS at once
    Close #1

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from threading import Thread
+from Queue import Queue
 from string import digits, uppercase
 from itertools import product
-import sched, time, requests
+import sched
+import time
+import requests
 from os import path
 from progress.bar import IncrementalBar
 
@@ -11,62 +15,56 @@ from progress.bar import IncrementalBar
 
 URL = 'http://cstrips.bitstrips.com/%s_USER.png'
 WAITTIME = 0.05
+CONCURRENCY = 20
 
 bar = IncrementalBar('Processing', max=60466176)
 
 """ download images """
-def dwIMG(p_sID):
-    bar.next()
 
+
+def dwIMG(p_sID):
     sURL = URL % p_sID
-    #print sURL,
+    idPath = p_sID + '.png'
+    if path.exists(idPath):
+        print(idPath, "already exists, skipping")
+        return
+
+    # print sURL,
     r = requests.get(sURL)
 
     if r.status_code == 200:
-        f = open(p_sID + '.png','wb')
+        f = open(idPath, 'wb')
         f.write(r.content)
         f.close()
-        print ' YES'
-        #print ' NONE'
+
 
 def genIDS():
+    q = Queue(CONCURRENCY * 2)
+
+    def doWork():
+        while True:
+            id = q.get()
+            try:
+                dwIMG(id)
+            except:
+                print("error", id)
+            q.task_done()
+
+    for i in range(CONCURRENCY):
+        t = Thread(target=doWork)
+        t.daemon = True
+        t.start()
+
+    print("computing ids")
     chars = digits + uppercase
-    iLast = getLAST()
-    iCurrent = -1
     for sID in product(chars, repeat=5):
-        iCurrent += 1
+        q.put(''.join(sID))
+        bar.next()
 
-        # Ignore already tested IDs
-        if iCurrent < iLast:
-            continue
+    print("done computing")
 
-        # Save each 1000 iterations
-        if not iCurrent % 1000:
-            savLAST(iCurrent)
+    q.join()
 
-        dwIMG(''.join(sID))
-        time.sleep(WAITTIME)
-
-# ----------------- recover last session
-FILELAST = 'session.last'
-
-def savLAST(p_iNum):
-    f = open(FILELAST,'w')
-    f.write(str(p_iNum))
-    f.close()
-    print "** saved session [", p_iNum, "] **"
-
-
-def getLAST():
-    if path.isfile(FILELAST):
-        f = open(FILELAST,'r')
-        iNum = int(f.read(), 10)
-        print "recover from:", iNum
-        f.close()
-        return iNum
-    else:
-        print "process started..."
-        return 0
 
 if __name__ == '__main__':
     genIDS()
